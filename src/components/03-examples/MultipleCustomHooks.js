5e03319ea6cde422d2e94a5cbb1b1036c1757b7c
devdnia <devdnia@gmail.com>
2021-11-19 09:53:20 +0100

    Primer commit

---
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useFetch } from '../../hooks/useFetch';
+import { useCounter } from '../../hooks/useCounter';
+
+import './examples.css';
+
+export const MultipleCustomHooks = () => {
+
+    const { counter, increment } = useCounter(1);
+
+    const { loading, data } = useFetch( `https://www.breakingbadapi.com/api/quotes/${ counter }`);
+    
+    const { author, quote } = !!data && data[0];
+
+    return (
+        <div>
+            <h1>BreakingBad Quotes</h1>
+            <hr />
+
+        {
+            loading
+            ?
+            (
+                <div className="alert alert-info text-center">
+                    Loading. . .
+                </div>
+            )
+            :
+            (
+
+                <blockquote className="blockquote text-right">
+                    <p className="mb-3">{ quote }</p>
+                    <footer className="blockquote-footer">{ author }</footer>
+                </blockquote>
+            )
+        }
+
+        <button 
+            className="btn btn-primary"
+            onClick={ increment }
+        >
+            Siguiente frase
+        </button>
+        </div>
+    )
+}
